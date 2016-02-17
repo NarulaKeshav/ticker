ab73ba01994e353efca7ff57769423b92948c3d5
Keshav Narula <NarulaKeshav13@gmail.com>
2016-02-16 18:17:20 -0800

    Initiate WOW JS

--- js/ticker.js
+++ js/ticker.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 
     // Initializing WOW JS
     new WOW().init();
+    var d = document;
+    var stopwatchTimer = d.getElementById("stopwatch-timer");
+    var start = d.getElementById("btn-start");
+
+    // Stopwatch
+
 });
